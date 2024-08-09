c3b76d141314ce8a29918cb0ad3f28798b313bc6
Trevor Gravely <trevor.gravely@vanderbilt.edu>
2024-08-09 15:41:33 -0500

    Added input parsing.

--- main.c
+++ main.c
@@ -2,6 +2,47 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define VECTOR_SIZE 784
+#define MAX_LINE_LENGTH 4096
+
+// Function to trim newline characters from a string
+void trim_newline(char *str) {
+  size_t len = strlen(str);
+  if (len > 0 && str[len - 1] == '\n') {
+    str[len - 1] = '\0';
+  }
+}
+
+void read_in_training_data(int **images, int *labels, int num_training_images) {
+  const char *csv_file_path = "/usr/src/myapp/mnist_handwritten_train.csv";
+  FILE *file = fopen(csv_file_path, "r");
+  if (!file) {
+    perror("File opening failed");
+  }
+
+  int image_idx = 0;
+  char line[MAX_LINE_LENGTH];
+  while (fgets(line, sizeof(line), file) && image_idx < num_training_images) {
+    trim_newline(line);
+
+    char *token = strtok(line, ",");
+    int i = 0;
+
+    for (; i < VECTOR_SIZE && token != NULL; i++) {
+      images[image_idx][i] = atoi(token);
+      token = strtok(NULL, ",");
+    }
+
+    if (token != NULL) {
+      labels[image_idx] = atoi(token);
+    } else {
+      perror("Missing label");
+    }
+
+    image_idx++;
+  }
+}
+
 float **allocate_matrix(int rows, int cols) {
   float **mat = (float **)malloc(rows * sizeof(float *));
   if (mat == NULL) {
@@ -29,6 +70,7 @@ int main() {
   int layer_sizes[] = {784, 16, 16, 10};
   int num_layers = sizeof(layer_sizes) / sizeof(layer_sizes[0]);
 
+  // First, we allocate the weight matrices.
   float ***weights = (float ***)malloc((num_layers - 1) * sizeof(float **));
   if (weights == NULL) {
     perror("malloc");
@@ -47,19 +89,38 @@ int main() {
     }
   }
 
-  for (int i = 1; i < 2; i++) {
-    for (int j = 0; j < layer_sizes[i]; j++) {
-      for (int k = 0; k < layer_sizes[i + 1]; k++) {
-        printf("%f", weights[i][j][k]);
-      }
-      printf("\n");
-    }
+  // Next, we need to read in the training data.
+  int num_training_images = 3;
+  int num_pixels_per_image = 784;
+  int **images = (int **)malloc(num_training_images * sizeof(int *));
+  int *labels = (int *)malloc(num_training_images * sizeof(int));
+  for (int i = 0; i < num_training_images; i++) {
+    images[i] = (int *)malloc(num_pixels_per_image * sizeof(int));
   }
 
+  read_in_training_data(images, labels, num_training_images);
+
+  for (int i = 0; i < num_training_images; i++) {
+    printf("leading data: ");
+    for (int j = 0; j < 10; j++) {
+      printf("%d ", images[i][j]);
+    }
+    printf("\n");
+    printf("label: %d \n", labels[i]);
+  }
+
+  // CLEAN UP
   for (int i = 0; i < num_layers - 1; i++) {
     free_matrix(weights[i], layer_sizes[i]);
   }
   free(weights);
+
+  for (int i = 0; i < num_training_images; i++) {
+    free(images[i]);
+  }
+  free(images);
+
+  free(labels);
 
   return 0;
 }
